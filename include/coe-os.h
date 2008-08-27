12f5d4fce3163b7df63d5ae7f5063253370294f9
Waldemar Rachwal <waldemar.rachwal@gmail.com>
2008-08-27 23:53:19 +0000

    RWLock, its Guard, and CondVar added.

--- include/coe-os.h
+++ include/coe-os.h
@@ -16,16 +16,16 @@ private:
     _Noncopyable& operator= (const _Noncopyable&);
 };
 
-// =======================================================================
-// Guard
+// -----------------------------------------------------------------------
+// _Guard
 
-struct _SpyGuard;
+struct _GuardSpy;
 
-class SynchGuard : private _Noncopyable {
+class _Guard : private _Noncopyable {
 public:
-    virtual ~SynchGuard ();
+    virtual ~_Guard ();
 protected:
-    friend struct _SpyGuard;
+    friend struct _GuardSpy;
     virtual void _unlock () = 0;
     bool         _locked;
 };
@@ -38,20 +38,70 @@ public:
     Mutex ();
     ~Mutex ();
 
-    class Guard : public SynchGuard {
+    static Mutex& from_sys (pthread_mutex_t& mutex);
+
+    class Guard : public _Guard {
     public:
         Guard (Mutex& mutex);
-        Guard (Mutex& mutex, SynchGuard& parent);   // lock chaining
+        Guard (Mutex& mutex, _Guard& parent);   // lock chaining
         ~Guard ();
-    private:
+    //FIXME: private:
         virtual void _unlock ();
         Mutex&  _mutex;
     };
 
-private:
+//FIXME: private:
     pthread_mutex_t _mutex;
     void   _lock ();
     void _unlock ();
+};
+
+// =======================================================================
+// RWLock
+
+class RWLock : private _Noncopyable {
+public:
+    RWLock ();
+    ~RWLock ();
+
+    static RWLock& from_sys (pthread_rwlock_t& rwlock);
+
+    enum Type { RLock, WLock };
+
+    class Guard : public _Guard {
+    public:
+        Guard (RWLock& rwlock, Type type);
+        Guard (RWLock& rwlock, Type type, _Guard& parent);  // lock chaining
+        ~Guard ();
+    private:
+        virtual void _unlock ();
+        RWLock& _rwlock;
+    };
+
+private:
+    pthread_rwlock_t    _rwlock;
+    void   _lock (Type type);
+    void _unlock ();
+};
+
+// =======================================================================
+// CondVar
+
+class CondVar : private _Noncopyable {
+public:
+    CondVar ();
+    ~CondVar ();
+
+    static CondVar& from_sys (pthread_cond_t& cond);
+
+    void wait      (Mutex::Guard& guard);
+    bool timedwait (Mutex::Guard& guard, const timespec& abstime);
+
+    void signal    ();
+    void broadcast ();
+
+private:
+    pthread_cond_t  _cond;
 };
 
 // =======================================================================
--- src/coe-os.cpp
+++ src/coe-os.cpp
@@ -1,6 +1,7 @@
 // coe-os.cpp
 
 #include "coe-os.h"
+#include <errno.h>
 #include <iostream>
 
 using namespace std;
@@ -16,18 +17,23 @@ using namespace std;
 
 
 // =======================================================================
-// SynchGuard
+// _Guard
 
-SynchGuard::~SynchGuard ()
+_Guard::~_Guard ()
 {
 }
 
 // -----------------------------------------------------------------------
 // _SpyGuard
 
-struct _SpyGuard {
-    static void unlock (SynchGuard& guard) { guard._unlock(); }
+struct _GuardSpy {
+    static void unlock (_Guard& guard);
 };
+
+void _GuardSpy::unlock (_Guard& guard)
+{
+    guard._unlock();
+}
 
 // =======================================================================
 // Mutex
@@ -46,6 +52,11 @@ Mutex::~Mutex ()
     if (status != 0) {
         raise_error(status, "pthread_mutex_destroy");
     }
+}
+
+Mutex& Mutex::from_sys (pthread_mutex_t& mutex)
+{
+    return reinterpret_cast<Mutex&>(mutex);
 }
 
 void Mutex::_lock ()
@@ -73,13 +84,13 @@ Mutex::Guard::Guard (Mutex& mutex) : _mutex(mutex)
     _mutex._lock();
 }
 
-Mutex::Guard::Guard (Mutex& mutex, SynchGuard& parent) : _mutex(mutex)
+Mutex::Guard::Guard (Mutex& mutex, _Guard& parent) : _mutex(mutex)
 {
     _locked = true;
     _mutex._lock();
     // lock chaining:
     //   once current `_mutex' is locked, unlock the `parent'
-    _SpyGuard::unlock(parent);
+    _GuardSpy::unlock(parent);
 }
 
 Mutex::Guard::~Guard ()
@@ -95,18 +106,164 @@ void Mutex::Guard::_unlock ()
     }
 }
 
+// =======================================================================
+// RWLock
+
+RWLock::RWLock ()
+{
+    int status = pthread_rwlock_init(&_rwlock, NULL);
+    if (status != 0) {
+        raise_error(status, "pthread_rwlock_init");
+    }
+}
+
+RWLock::~RWLock ()
+{
+    int status = pthread_rwlock_destroy(&_rwlock);
+    if (status != 0) {
+        raise_error(status, "pthread_rwlock_destroy");
+    }
+}
+
+RWLock& RWLock::from_sys (pthread_rwlock_t& rwlock)
+{
+    return reinterpret_cast<RWLock&>(rwlock);
+}
+
+void RWLock::_lock (Type type)
+{
+    if (RLock == type) {
+        int status = pthread_rwlock_rdlock(&_rwlock);
+        if (status != 0) {
+            raise_error(status, "pthread_rwlock_rdlock");
+        }
+    }
+    else {
+        int status = pthread_rwlock_wrlock(&_rwlock);
+        if (status != 0) {
+            raise_error(status, "pthread_rwlock_wrlock");
+        }
+    }
+}
+
+void RWLock::_unlock ()
+{
+    int status = pthread_rwlock_unlock(&_rwlock);
+    if (status != 0) {
+        raise_error(status, "pthread_rwlock_unlock");
+    }
+}
+
+// -----------------------------------------------------------------------
+// RWLock::Guard
+
+RWLock::Guard::Guard (RWLock& rwlock, Type type) : _rwlock(rwlock)
+{
+    _locked = true;
+    _rwlock._lock(type);
+}
+
+RWLock::Guard::Guard (RWLock& rwlock, Type type, _Guard& parent) : _rwlock(rwlock)
+{
+    _locked = true;
+    _rwlock._lock(type);
+    // lock chaining:
+    //   once current `_rwlock' is locked, unlock the `parent'
+    _GuardSpy::unlock(parent);
+}
+
+RWLock::Guard::~Guard ()
+{
+    _unlock();  //FIXME: pure virtual, or scoped?
+}
+
+void RWLock::Guard::_unlock ()
+{
+    if (_locked) {
+        _locked = false;
+        _rwlock._unlock();
+    }
+}
+
+// =======================================================================
+// CondVar
+
+CondVar::CondVar ()
+{
+    int status = pthread_cond_init(&_cond, NULL);
+    if (status != 0) {
+        raise_error(status, "pthread_cond_init");
+    }
+}
+
+CondVar::~CondVar ()
+{
+    int status = pthread_cond_init(&_cond, NULL);
+    if (status != 0) {
+        raise_error(status, "pthread_cond_init");
+    }
+}
+
+CondVar& CondVar::from_sys (pthread_cond_t& cond)
+{
+    return reinterpret_cast<CondVar&>(cond);
+}
+
+void CondVar::wait (Mutex::Guard& guard)
+{
+    int status = pthread_cond_wait(&_cond, &guard._mutex._mutex);
+    if (status != 0) {
+        raise_error(status, "pthread_cond_wait");
+    }
+}
+
+bool CondVar::timedwait (Mutex::Guard& guard, const timespec& abstime)
+{
+    int status = pthread_cond_timedwait(&_cond, &guard._mutex._mutex, &abstime);
+    if (status == 0) {
+        return true;
+    }
+    else
+    if (status == ETIMEDOUT) {
+        return false;
+    }
+    else {
+        raise_error(status, "pthread_cond_timedwait");
+    }
+}
+
+void CondVar::signal ()
+{
+    int status = pthread_cond_signal(&_cond);
+    if (status != 0) {
+        raise_error(status, "pthread_cond_signal");
+    }
+}
+
+void CondVar::broadcast ()
+{
+    int status = pthread_cond_broadcast(&_cond);
+    if (status != 0) {
+        raise_error(status, "pthread_cond_broadcast");
+    }
+}
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-int main ()
+static pthread_mutex_t  mutex_0  =  PTHREAD_MUTEX_INITIALIZER;
+static pthread_rwlock_t rwlock_0 = PTHREAD_RWLOCK_INITIALIZER;
+
+static void check_guards_usage ()
 {
     // lock hierarchy
     {
         Mutex   mutex_A;
-        Mutex   mutex_B;
+        RWLock  rwlock_B;
         Mutex   mutex_C;
         {
+            Mutex::Guard    guard_0(Mutex::from_sys(mutex_0));
             Mutex::Guard    guard_A(mutex_A);
-            Mutex::Guard    guard_B(mutex_B);
+            RWLock::Guard   guard_B(rwlock_B, RWLock::RLock);
             Mutex::Guard    guard_C(mutex_C);
         }
     }
@@ -117,10 +274,99 @@ int main ()
         Mutex   mutex_B;
         Mutex   mutex_C;
         {
-            Mutex::Guard    guard_A(mutex_A);
+            RWLock::Guard   guard_0(RWLock::from_sys(rwlock_0), RWLock::WLock);
+            Mutex::Guard    guard_A(mutex_A, guard_0);
             Mutex::Guard    guard_B(mutex_B, guard_A);
             Mutex::Guard    guard_C(mutex_C, guard_B);
         }
     }
+}
+
+// -----------------------------------------------------------------------
+// Programming with POSIX Threads, by David R.Butenhof
+// [cond.c] sec-3.3.2, p.77
+
+static struct my_struct_t {
+    Mutex   mutex;  // protects access to value
+    CondVar cond;   // signals change to value
+    int     value;  // access protected by mutex
+    // -----------------
+    my_struct_t () : value(0) {}
+} g_data;
+
+static int hibernation = 1;     // default to 1 second
+
+/*
+ * Thread start routine. It will set the main thread's predicate
+ * and signal the conditional variable.
+ */
+static void*
+wait_thread (void* arg)
+{
+    sleep(hibernation);
+
+    Mutex::Guard    guard(g_data.mutex);
+
+    g_data.value = 1;   // set predicate
+    g_data.cond.signal();
+
+    return NULL;
+}
+
+int main (int argc, char* argv[])
+{
+    check_guards_usage();
+
+    /*
+     * If an argument is specified, interpret it as the number
+     * of seconds for wait_thread to sleep before signaling the
+     * conditional variable. You can play with this to see the
+     * condition wait below time out or wake normally.
+     */
+    if (argc > 1)
+        hibernation = atoi(argv[1]);
+
+    /*
+     * Create wait_thread.
+     */
+    pthread_t   wait_thread_id;
+    int status = pthread_create(&wait_thread_id, NULL, wait_thread, NULL);
+    if (status != 0) {
+        raise_error(status, "Create wait thread");
+    }
+
+    /*
+     * Wait on the condition variable for 2 seconds, or until
+     * signaled by the wait_thread. Normally, wait_thread
+     * should signal. If you raise "hibernation" above 2
+     * seconds, it will time out.
+     */
+    timespec    timeout;
+    timeout.tv_sec  = time(NULL) + 2;
+    timeout.tv_nsec = 0;
+
+    Mutex::Guard    guard(g_data.mutex);
+
+    while (g_data.value == 0) {
+
+        if (g_data.cond.timedwait(guard, timeout)) {
+
+            if (g_data.value != 0) {    // check predicate
+                cout << "Condition was signaled." << endl;
+            }
+        }
+        else {
+            cout << "Condition wait timed out." << endl;
+            // [under cygwin]
+            // --------------
+            // Without the following break statement when hibernation is 3 or longer,
+            // the execution stays in the while loop forever.
+            // Apparently, second and next timedwait(s) monopolize CPU completely,
+            // and condition variable in the wait_thread is never signaled.
+            break;
+        }
+    }
+
+    return 0;
 }
 
