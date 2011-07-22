68ad82a70c545573061ab5c980b2301a9514fc57
Waldemar Rachwal <waldemar.rachwal@gmail.com>
2011-07-22 18:41:38 +0200

    Recursively 'assign_arg_info' in ArgListI.
    (BTW, track added files in MANIFEST).

--- test/meta.cpp
+++ test/meta.cpp
@@ -31,8 +31,22 @@ namespace coe {
 // ArgListI
 
 struct ArgListI {
-    const ArgI**    arg;
-    size_t          len;
+    const Meta<ArgI>**  arg;
+    size_t              len;
+};
+
+template<class List> struct assign_arg_info;
+template<>
+struct assign_arg_info<Nil> {
+    static void apply (const Meta<ArgI>**) {}
+};
+template<class Head, class Tail>
+struct assign_arg_info<Cons<Head, Tail> > {
+    static void apply (const Meta<ArgI>** argi)
+        {
+            *argi = Ctti<Head, ArgI>::meta();
+            assign_arg_info<Tail>::apply(++argi);
+        }
 };
 
 namespace coe {
@@ -40,10 +54,10 @@ namespace coe {
     struct init_meta_info<List, ArgListI> {
         void operator() (ArgListI& info) const
             {
-                static const ArgI*  args[Length<List>::value + 1];
+                static const Meta<ArgI>*    args[Length<List>::value + 1];
                 info.arg = args;
                 info.len = Length<List>::value;
-                // initialize args[...]
+                assign_arg_info<List>::apply(args);
             }
     };
 }
@@ -61,10 +75,21 @@ int main ()
     for (const Meta<ArgI>* meta = Meta<ArgI>::registry(); meta; meta = meta->next)
         cout << "ArgI#" << meta->indx << " -- " << meta->info.type.name() << endl;
 
-    typedef List3<int, short, string>::type A_3;
-    cout << "length of 3 items list = " << Length<A_3>::value << endl;
+    typedef List3<int, short, string>::type A_3a;
+    typedef List3<int, long, double>::type A_3b;
 
-    Ctti<A_3, ArgListI>::meta();
+    cout << "length of 3 items list = " << Length<A_3a>::value << endl;
+
+    Ctti<A_3a, ArgListI>::meta();
+    Ctti<A_3b, ArgListI>::meta();
+
+    for (const Meta<ArgListI>* ml = Meta<ArgListI>::registry(); ml; ml = ml->next) {
+        cout << "ArgListI#" << ml->indx << " -- args (" << ml->info.len << ")" << endl;
+        for (size_t i = 0; i < ml->info.len; ++i) {
+            const Meta<ArgI>    *ma = ml->info.arg[i];
+            cout << "  - [" << i << "] -- " << ma->info.type.name() << " #" << ma->indx << endl;
+        }
+    }
 
     return 0;
 }
