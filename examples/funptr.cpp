05ae18743f8e66f9298994e63534755a91758d31
Waldemar Rachwal <waldemar.rachwal@gmail.com>
2012-03-07 19:26:03 +0100

    OnEv<..> -- flexible & powerful interface.
    Yet the implementation is very clear! :)
    
    After initial OnEv<> all successive
    metafunctions may be called in any order.
    Constrains are obviously taken into account
    (see commented //XXX error: in the main())
    and they're reevaluated after each call.
    
    At the moment implemented:
    - guard<Obj, &Obj::fun>
    - defer
    - to<State | List>
    - react<Obj, &Obj::fun>
    
    The solution is so powerful that I am looking
    forward to fit the remaining wishes like joins.
    
    Obviously, this is "only" the interface part--
    --the runtime needs still to be worked out.
    
    
    === $ make examples/funptr && ./examples/funptr
    
    (void*)Gfun::type(&g2a) ==> 0x8049270
    sizeof(Gfun::type(&g2a)) ==> 4
    Mfun::type(&Cls::m2a) ==> 1
    sizeof(Mfun::type(&Cls::m2a)) ==> 8
    (MFUN<void> ::ptr(&Cls::m0a)) ==> 1
    (MFUN<void, CoePfx>::ptr(&Cls::h2a)) ==> 1
    (MFUN<void, CoePfx>::ptr(&Cls::h2A)) ==> 1
    /*** calling plus_fi(f=7, i=6) ***/
    execute<double>(MFUN<double>::ptr(&Cls::plus_fi), 2, &obj, argfi) ==> 13
    (Length<List2<A1, A2>::type>::value) ==> 2
    @@@ OnEv<> -- transitions interface
    (Length<OnEv<>::type::EV::args_type>::value) ==> 0
    ONEV(OnEv<>::type::optset) ==> ()
    (Length<OnEv<ev12>::type::EV::args_type>::value) ==> 2
    ONEV(OnEv<ev12>::type::optset) ==> (EVENT)
    ONEV(OnEv<ev12>::guard<Cls, &Cls::h2a>::type::optset) ==> (EVENT GUARD)
    ONEV(OnEv<ev12>::react<Cls, &Cls::h2a>::type::optset) ==> (EVENT REACT)
    ONEV(OnEv<ev12>::guard<Cls, &Cls::h2a>::react<Cls, &Cls::h2a>::type::optset) ==> (EVENT GUARD REACT)
    ONEV(OnEv<ev12>::react<Cls, &Cls::h2a>::guard<Cls, &Cls::h2a>::type::optset) ==> (EVENT GUARD REACT)
    (OnEv<ev12>::react<Cls, &Cls::h2a>::guard<Cls, &Cls::h2a>::type::react_fun_ptr()) ==> 1
    ONEV(OnEv<ev12>::defer::type::optset) ==> (EVENT DEFER)
    ONEV(OnEv<>::to<A2>::type::optset) ==> (TO)
    ONEV(OnEv<>::guard<Cls, &Cls::h0a>::to<A2>::type::optset) ==> (GUARD TO)
    ONEV(OnEv<ev12>::to<A2>::type::optset) ==> (EVENT TO)

--- examples/funptr.cpp
+++ examples/funptr.cpp
@@ -190,51 +190,154 @@ struct MFUN {
 };
 
 // ---------------------------------------------------------------------------
-// OnEv<EV> [::react<Obj, &Obj::fun> | ...] ::type
 
-#if 0
-template<class _EV>
-struct OnEv {
-    typedef _EV EV;
-    enum { has_target = false };
-    enum { has_react  = false };
-    enum { has_guard  = false };
+namespace detail {
+
+template<class Result, int ApiSet> struct onev_apiset;
+
+enum {
+    api_EVENT = (1 << 0),
+    api_GUARD = (1 << 1),
+    api_DEFER = (1 << 2),
+    api_TO    = (1 << 3),
+    api_REACT = (1 << 4)
+};
+
+const int api_ONEV =
+    api_GUARD
+  | api_DEFER
+  | api_TO
+  | api_REACT
+  ;
+
+const int api_NOEV =
+    api_GUARD
+  | api_TO
+  | api_REACT
+  ;
+
+// ------------------------------------
+// guard<Obj, &Obj::fun>
+
+template<class Result, int ApiSet, bool _Define = (0 != (ApiSet & api_GUARD))>
+struct onev_api_guard {
+
     template<class Obj,
-             typename handler_type<typename _EV::args_type>::template mem_fun<Obj>::type fun>
-    struct react {
-        typedef typename handler_type<typename _EV::args_type>::template mem_fun<Obj>::type react_fun_type;
+             typename /*FIXME*/handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type fun>
+    struct _result : Result {
+        enum { optset = Result::optset | api_GUARD };
+        typedef typename /*FIXME*/handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type guard_fun_type;
+        static guard_fun_type guard_fun_ptr () { return fun; }
+    };
+    enum { _apiset = ApiSet & ~api_GUARD };
+
+    template<class Obj,
+             typename /*FIXME*/handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type fun>
+    struct guard : onev_apiset<_result<Obj, fun>, _apiset> {
+        typedef _result<Obj, fun> type;
+    };
+};
+template<class Result, int ApiSet>
+struct onev_api_guard<Result, ApiSet, false> {};
+
+// ------------------------------------
+// defer
+
+template<class Result, int ApiSet, bool _Define = (0 != (ApiSet & api_DEFER))>
+struct onev_api_defer {
+
+    struct _result : Result {
+        enum { optset = Result::optset | api_DEFER };
+    };
+    enum { _apiset = ApiSet & ~(api_DEFER|api_TO|api_REACT) };
+
+    struct defer : onev_apiset<_result, _apiset> {
+        typedef _result type;
+    };
+};
+
+// ------------------------------------
+// to<State | List>
+
+template<class Result, int ApiSet, bool _Define = (0 != (ApiSet & api_TO))>
+struct onev_api_to {
+
+    template<class To>
+    struct _result : Result {
+        enum { optset = Result::optset | api_TO };
+        //TODO static check: single state | list of states. doable?
+        typedef To target;
+    };
+    enum { _apiset = ApiSet & ~(api_TO|api_DEFER) };
+
+    template<class To>
+    struct to : onev_apiset<_result<To>, _apiset> {
+        typedef _result<To> type;
+    };
+};
+template<class Result, int ApiSet>
+struct onev_api_to<Result, ApiSet, false> {};
+
+// ------------------------------------
+// react<Obj, &Obj::fun>
+
+template<class Result, int ApiSet, bool _Define = (0 != (ApiSet & api_REACT))>
+struct onev_api_react {
+
+    template<class Obj,
+             typename handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type fun>
+    struct _result : Result {
+        enum { optset = Result::optset | api_REACT };
+        typedef typename handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type react_fun_type;
         static react_fun_type react_fun_ptr () { return fun; }
-        typedef _EV EV;
-        enum { has_target = false };
-        enum { has_react  = true  };
-        enum { has_guard  = false };
     };
-};
-#else
-template<class _EV>
-struct OnEv {
-    struct type {   // result
-        typedef _EV EV;
-        enum { has_target = false };
-        enum { has_react  = false };
-        enum { has_guard  = false };
-    };
-    typedef type _type1;    // alias
+    enum { _apiset = ApiSet & ~(api_REACT|api_DEFER) };
 
     template<class Obj,
-             typename handler_type<typename _EV::args_type>::template mem_fun<Obj>::type fun>
-    struct react {
-        struct type : _type1 {  // result
-            typedef typename handler_type<typename _type1::EV::args_type>::template mem_fun<Obj>::type react_fun_type;
-            enum { has_react = true  };
-            static react_fun_type react_fun_ptr () { return fun; }
-        };
-        typedef type _type2;    // alias
-
-        ///XXX nested api(s) for OnEv/react ...
+             typename handler_type<typename Result::EV::args_type>::template mem_fun<Obj>::type fun>
+    struct react : onev_apiset<_result<Obj, fun>, _apiset> {
+        typedef _result<Obj, fun> type;
     };
 };
-#endif
+template<class Result, int ApiSet>
+struct onev_api_react<Result, ApiSet, false> {};
+
+// ------------------------------------
+
+template<class Result, int ApiSet>
+struct onev_apiset
+    :  onev_api_guard<Result, ApiSet>
+    ,  onev_api_defer<Result, ApiSet>
+    ,  onev_api_to   <Result, ApiSet>
+    ,  onev_api_react<Result, ApiSet>
+{
+    typedef Result type;
+};
+
+// ---
+
+struct _noev : event<> {};  // fictitious, allows to reuse onev_api_.. in (on|no)ev contexts
+
+template<class _EV>
+struct result_onev {
+    typedef _EV EV;
+    enum { optset = (meta::IsSame_<EV, _noev>::value ? 0 : api_EVENT) };
+};
+
+}   // namespace detail
+
+// ------------------------------------
+// OnEv<EV> [::onev-detail<..> ...] ::type
+// OnEv<>   [::noev-detail<..> ...] ::type
+
+template<class _EV = detail::_noev>
+struct OnEv : detail::onev_apiset<detail::result_onev<_EV>, detail::api_ONEV> {
+    typedef detail::result_onev<_EV> type;
+};
+template<>
+struct OnEv<detail::_noev> : detail::onev_apiset<detail::result_onev<detail::_noev>, detail::api_NOEV> {
+    typedef detail::result_onev<detail::_noev> type;
+};
 
 // ---------------------------------------------------------------------------
 
@@ -246,6 +349,7 @@ struct ev12 : event<A1, A2> {};
 struct Cls {
     void m0a () {}
     void m2a (A1&, A2&) {}
+    void h0a (Kernel&) {}
     void h2a (Kernel&, A1&, A2&) {}
     void h2A (Kernel&, A1&, A2&) const {}
     double plus_fi (float& f, int& i)
@@ -262,6 +366,31 @@ void g2a (A1&, A2&) {}
 #define EXPR_(expr) do { cout << #expr << " ==> " << (expr) << endl; } while(0)
 
 typedef Cons<Kernel, Nil> CoePfx;
+
+// ---------------------------------------------------------------------------
+
+static void _strapp (string& str, const char* app, bool cond)
+{
+    if (cond) {
+        if (! str.empty())
+            str += ' ';
+        str += app;
+    }
+}
+
+static string ONEV (int optset)
+{
+    string  str;
+    #define ONEV_API_(name) #name, (optset & detail::api_##name)
+    _strapp(str, ONEV_API_(EVENT));
+    _strapp(str, ONEV_API_(GUARD));
+    _strapp(str, ONEV_API_(DEFER));
+    _strapp(str, ONEV_API_(TO));
+    _strapp(str, ONEV_API_(REACT));
+    return '(' + str + ')';
+}
+
+// ===========================================================================
 
 int main ()
 {
@@ -294,16 +423,28 @@ int main ()
     EXPR_((Length<List2<A1, A2>::type>::value));
 
     // === explore alternative interface for transitions
+    cout << "@@@ OnEv<> -- transitions interface" << endl;
 
+    EXPR_((Length<OnEv<>::type::EV::args_type>::value));
+    EXPR_(ONEV(OnEv<>::type::optset));
     EXPR_((Length<OnEv<ev12>::type::EV::args_type>::value));
-    EXPR_((OnEv<ev12>::type::has_guard));
-    EXPR_((OnEv<ev12>::type::has_target));
-    EXPR_((OnEv<ev12>::type::has_react));
+    EXPR_(ONEV(OnEv<ev12>::type::optset));
+    EXPR_(ONEV(OnEv<ev12>::guard<Cls, &Cls::h2a>::type::optset));
+    EXPR_(ONEV(OnEv<ev12>::react<Cls, &Cls::h2a>::type::optset));
+    EXPR_(ONEV(OnEv<ev12>::guard<Cls, &Cls::h2a>::react<Cls, &Cls::h2a>::type::optset));
+    EXPR_(ONEV(OnEv<ev12>::react<Cls, &Cls::h2a>::guard<Cls, &Cls::h2a>::type::optset));
+    EXPR_((OnEv<ev12>::react<Cls, &Cls::h2a>::guard<Cls, &Cls::h2a>::type::react_fun_ptr()));
+    EXPR_(ONEV(OnEv<ev12>::defer::type::optset));
 
-    EXPR_((Length<OnEv<ev12>::react<Cls, &Cls::h2a>::type::EV::args_type>::value));
-    EXPR_((OnEv<ev12>::react<Cls, &Cls::h2a>::type::has_guard));
-    EXPR_((OnEv<ev12>::react<Cls, &Cls::h2a>::type::has_target));
-    EXPR_((OnEv<ev12>::react<Cls, &Cls::h2a>::type::has_react));
-    EXPR_((OnEv<ev12>::react<Cls, &Cls::h2a>::type::react_fun_ptr()));
+    //XXX error: ‘react’ is not a member of ‘detail::onev_api_defer<detail::result_onev<ev12>, 14, true>::defer’
+    //EXPR_(ONEV(OnEv<ev12>::defer::react<Cls, &Cls::h2a>::type::optset));
+
+    EXPR_(ONEV(OnEv<>::to<A2>::type::optset));
+
+    //XXX error: could not convert template argument ‘&Cls::h2a’ to ‘void (Cls::*)(coe::Kernel&)’
+    //EXPR_(ONEV(OnEv<>::guard<Cls, &Cls::h2a>::to<A2>::type::optset));
+
+    EXPR_(ONEV(OnEv<>::guard<Cls, &Cls::h0a>::to<A2>::type::optset));
+    EXPR_(ONEV(OnEv<ev12>::to<A2>::type::optset));
 }
 
